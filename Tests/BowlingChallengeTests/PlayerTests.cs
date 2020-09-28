5970c11397455c92c7b1ee64a73cb48b460414af
Kristen Howton <kristen.howton@gmail.com>
2020-09-28 13:32:25 -0500

    created a test to test the name of a player

---
+++ Tests/BowlingChallengeTests/PlayerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingChallengeTests
+{
+    [TestClass]
+    public class PlayerTests
+    {
+        [TestMethod]
+        public void NameValid()
+        {
+            //Arrange
+            //Instance of player class
+            Player player = new Player
+            {
+                //using object initatior
+                Name = "Jon",
+            };
+            string expected = "Jon";
+
+            //Act
+            string actual = player.Name;
+            //Assert 
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
