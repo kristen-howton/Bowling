8bd2a6d1746345a174cf42a4b6adb922b5473dc2
Kristen Howton <kristen.howton@gmail.com>
2020-09-28 15:50:11 -0500

    player repo retive valid

---
+++ Tests/BowlingChallengeTests/RepositoryTests/PlayerRepositoryTest.cs
@@ -0,0 +1,30 @@
+﻿using BowlingChallenge.Models;
+using BowlingChallenge.Repositories;
+using System;
+using Xunit;
+
+namespace BowlingChallengeTests.RepositoryTests
+{
+    public class PlayerRepositoryTest
+    {
+        [Fact]
+        public void RetriveValid()
+        {
+            var playerRepository = new PlayerRepository();
+            var expected = new Player(1)
+            {
+                Name = "Jon",
+            };
+            //Act
+            var actual = playerRepository.Retrieve(1);
+
+            //Assert 
+
+            Assert.Equal(expected.PlayerId, actual.PlayerId);
+            Assert.Equal(expected.Name, actual.Name);
+
+        }
+    }
+}
+
+
