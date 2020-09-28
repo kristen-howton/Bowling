f2f83949a8d736b57e6c1a5f73d7b23ab4962ed6
Kristen Howton <kristen.howton@gmail.com>
2020-09-28 13:06:19 -0500

    player model and repo craeted

---
+++ BowlingChallenge/Models/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingChallenge.Models
+{
+    public class Player
+    {
+        public Player() 
+        {
+
+        }
+        public Player(int playerId)
+        {
+            PlayerId = playerId;
+        }
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+
+        public bool Validate()
+        {
+            var isValid = true;
+            if (string.IsNullOrWhiteSpace(Name)) isValid = false;
+            return isValid;
+        }
+    }
+}
